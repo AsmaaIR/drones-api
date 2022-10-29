3791fe07f0717cf43c67b8d2b46381902ad6b6d7
asmaa.ibrahim <asmaaibrahim06@gmail.com>
2022-10-29 13:41:29 +0200

    add apis

---
+++ src/main/java/com/drone/drones/rest/DroneResource.java
@@ -0,0 +1,45 @@
+package com.drone.drones.rest;
+
+import com.drone.drones.common.DroneModel;
+import com.drone.drones.common.MedicationModel;
+import com.drone.drones.service.DroneService;
+import com.drone.drones.service.MedicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("drones")
+public class DroneResource {
+
+	@Autowired private DroneService droneService;
+	@Autowired private MedicationService medicationService;
+
+	@GetMapping
+	List<DroneModel> findDrones() {
+		return droneService.getDrones();
+	}
+
+	@PostMapping
+	void registerDrone(@RequestBody @Valid DroneModel droneModel){
+		droneService.registerDrone(droneModel);
+	}
+
+	@GetMapping("/available")
+	List<DroneModel> getAvailableDroneForLoading(){
+		return droneService.getAvailableDroneForLoading();
+	}
+
+	@GetMapping("{droneId}/battery-level")
+	double getBatteryLevelForDrone(@PathVariable("droneId")  long droneId){
+		return droneService.getBatteryLevelForDrone(droneId);
+	}
+
+
+	@GetMapping("{droneId}/medications")
+	List<MedicationModel> getMedicationsByDroneId(@PathVariable("droneId")  long droneId){
+		return medicationService.getMedicationsByDroneId(droneId);
+	}
+}
